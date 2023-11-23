703c47ad3d303de14aa4739dd267ebaf27d8030d
PavanKumar06 <spavankumard@gmail.com>
2023-11-23 02:35:52 -0500

    Revert "Revert "node changes""
    This reverts commit 0fa423832d2a88ceb640073f02d5338bd3b6371f.

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -2,36 +2,55 @@ import KanbasNavigation from "./KanbasNavigation";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
-import db from "./Database";
 import store from "./store";
 import { Provider } from "react-redux";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Kanbas() {
-  const [courses, setCourses] = useState(db.courses);
+  const URL = `${process.env.REACT_APP_BASE_URL}/api/courses`;
   const [course, setCourse] = useState({
-    name: "New Course Name",
-    number: "New Course Number",
-    startDate: "2023-10-10",
-    endDate: "2023-12-12",
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+    department: "new Department",
+    credits: 3
   });
-  const addNewCourse = () => {
-    setCourses([...courses, { ...course, _id: new Date().getTime().toString() }]);
+  const [courses, setCourses] = useState([]);
+
+  const addNewCourse = async () => {
+    const response = await axios.post(URL, course);
+    setCourses([response.data, ...courses]);
   };
-  const deleteCourse = (courseId) => {
-    setCourses(courses.filter((course) => course._id !== courseId));
+
+  const findAllCourses = async () => {
+    const response = await axios.get(URL);
+    setCourses(response.data);
   };
-  const updateCourse = () => {
+
+  const deleteCourse = async (course) => {
+    await axios.delete(`${URL}/${course._id}`);
+    setCourses(courses.filter((c) => c._id !== course._id));
+  };
+
+  const updateCourse = async () => {
+    const response = await axios.put(`${URL}/${course._id}`, course);
     setCourses(
       courses.map((c) => {
         if (c._id === course._id) {
-          return course;
+          return response.data;
         } else {
           return c;
         }
       })
     );
+    setCourse(course)
   };
+
+  useEffect(() => {
+    findAllCourses();
+  }, []);
 
   return (
     <Provider store={store}>
